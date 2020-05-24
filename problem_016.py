cc1654bcd9e0ab5f67f522984be4642870637d33
MellowMarshmallow <kyk.ab.ca@gmail.com>
2020-05-24 01:55:27 -0600

    Solution to power digit sum

---
+++ problem_016.py
@@ -0,0 +1,10 @@
+# Power digit sum
+
+def brute_force(power: int) -> int:
+    n, Sum = 2 ** power, 0
+    while n:
+        Sum, n = Sum + n % 10, n // 10
+    return Sum
+
+print(brute_force(1000)) # Output: 1366
+    
